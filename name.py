c573a7643da92c9f6494d4dc4f76b57b461f0a0b
Ashok Saravanan <90977640+AshokSaravanan222@users.noreply.github.com>
2023-02-01 18:32:59 -0600

    Add files via upload

---
+++ name.py
@@ -0,0 +1,6 @@
+import pyautogui
+pyautogui.confirm(text='Sandhya', title='Scan Student', buttons=['OK', 'Cancel'])
+pyautogui.click('find.png')
+pyautogui.write('san')
+pyautogui.press('enter') 
+pyautogui.press('f6') 
