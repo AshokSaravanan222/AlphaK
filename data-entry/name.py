9f362db9e48b6703dac3d109b7c0c44c61a31636
Ashok Saravanan <ashko.saravanan@gmail.com>
2023-02-09 19:18:49 -0600

    Added new methods for finding boxes

--- data-entry/name.py
+++ data-entry/name.py
@@ -7,12 +7,46 @@ with open('test.json') as file:
     data = json.load(file)
 
 
+def find_box(path):
+    box_location = pyautogui.locateOnScreen(path)
+    box = pyautogui.screenshot(region=box_location)
+    text = pytesseract.image_to_string(box).lower()
+    return box_location if text == text[:-4].split("/")[-1] else None
+
+
+def find_find():
+    return pyautogui.locateOnScreen('images/find.png')
+
+
+def find_time():
+    return find_box('images/time.png')
+
+
+def find_date():
+    return find_box('images/date.png')
+
+
+def find_numbers():
+    number_boxes = []
+    for i in range(1, 11):
+        number_boxes.append(find_box(f"images/{i}.png"))
+    return number_boxes
+
+
+def click(box):
+    center = pyautogui.center(box)
+    pyautogui.click(center[0], center[1])
+
+
+def create_search_term(student):
+    return student[0:3].lower()
+
+# TODO:
+
 def search(student):
-    pyautogui.click('find.png')
-    search_term = student[0:3].lower()
-    pyautogui.write(search_term)
-    pyautogui.press('enter')
-    pyautogui.press('f6')
+    click()
+    pyautogui.write(create_search_term(student))
+    pyautogui.press(['enter', 'f6'], interval=0.1)
 
 def check_dates():
         return True
