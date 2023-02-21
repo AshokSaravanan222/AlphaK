6bbfb1c884f8a0b60484d3154382defc70911cf8
Ashok Saravanan <90977640+AshokSaravanan222@users.noreply.github.com>
2023-02-20 18:49:54 -0600

    Update main.py

--- data-entry/main.py
+++ data-entry/main.py
@@ -2,11 +2,27 @@ import buttons
 import enter
 import search_and_close
 
-
-def search_student(student):
+def find_buttons():
     """
-    A function that selects a student on the main screen.
+    A function that finds all the buttons on the main screen and score card entry page. 
+    """
+    
+
+def search_student(student, find_button):
+    """
+    A function that opens a student from the main screen.
 
     :param student: the full name of the student that needs to be searched
     :return: None
     """
+    
+def adjust_calendar(cursor, refresh_button, date_range):
+    """
+    A function that adjusts the calender on the score entry page to have the correct dates when entering a student's data.
+    
+    :param date_range: tuple: (start_date, end_date), where parameters are Datetime obejcts that represent the date ranges of the scores to be entered
+    """
+    
+def enter_row():
+    
+
